ef025ee5691209cf7fe6f7ac0fdef3049333c028
hubluish <james2048@naver.com>
2025-05-15 01:15:08 +0900

    server.js, enter.js

--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,37 @@
-// server.js
-const mongoclient = require('mongodb').MongoClient;
+const express = require('express');
+const app = express();
+const { MongoClient } = require('mongodb');
 
 const url = 'mongodb+srv://james2048:2055@james2048.3i9tg4t.mongodb.net/?retryWrites=true&w=majority&appName=james2048';
 
-mongoclient.connect(url)
+let mydb;
+
+// MongoDB 연결
+MongoClient.connect(url)
   .then(client => {
     console.log('몽고DB 접속 성공');
-    
-    // 서버 실행
-    const express = require('express');
-    const app = express();
 
+    mydb = client.db('myboard');
+
+    // /list 요청 처리
+    app.get('/list', function (req, res) {
+      mydb.collection('post').find().toArray()
+        .then(result => {
+          console.log(result);  // 콘솔 출력
+          res.send(result);     // 클라이언트에 응답
+        })
+        .catch(error => {
+          console.error('데이터 조회 실패:', error);
+          res.status(500).send('서버 오류');
+        });
+    });
+
+    // 서버 시작
     app.listen(8080, function () {
       console.log('포트 8080으로 서버 대기중 ...');
     });
+
   })
   .catch(err => {
-    console.error('몽고DB 접속 실패', err);
+    console.error('몽고DB 접속 실패:', err);
   });
