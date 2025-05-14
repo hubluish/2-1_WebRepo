661fe619b32083c1b2c2473f0c8f8e8a455fc2fe
hubluish <james2048@naver.com>
2025-05-15 02:20:57 +0900

    List.ejs까지함

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+const db = require('node-mysql/lib/db');
+app.set('view engine', 'ejs');
 
 const url = 'mongodb+srv://james2048:2055@james2048.3i9tg4t.mongodb.net/?retryWrites=true&w=majority&appName=james2048';
 
@@ -13,18 +18,40 @@ MongoClient.connect(url)
 
     mydb = client.db('myboard');
 
-    // /list 요청 처리
-    app.get('/list', function (req, res) {
-      mydb.collection('post').find().toArray()
-        .then(result => {
-          console.log(result);  // 콘솔 출력
-          res.send(result);     // 클라이언트에 응답
-        })
-        .catch(error => {
-          console.error('데이터 조회 실패:', error);
-          res.status(500).send('서버 오류');
-        });
+    app.get('/enter', function (req, res) {
+      res.sendFile(path.join(__dirname, 'enter.html'));
     });
+
+    // /list 요청 처리
+    app.get('/list', function(req, res){
+      // conn.query("select * from post", function (err, rows, fields) {
+      //   if (err) throw err;
+      //   console.log(rows);
+      // });
+
+      mydb.collection('post').find().toArray(function(err, result){
+        console.log(result);
+      });
+      res.render('list.ejs');
+    });
+
+
+    app.post('/save', function(req, res){
+      console.log(req.body.title);
+      console.log(req.body.content);
+
+  // 몽고DB에 데이터 저장하기
+    mydb.collection('post').insertOne(
+      { title: req.body.title, content: req.body.content }
+    ).then(result => {
+      console.log(result);
+      console.log('데이터 추가 성공');
+    });
+
+    res.send('데이터 추가 성공');
+  });
+
+
 
     // 서버 시작
     app.listen(8080, function () {
@@ -35,3 +62,5 @@ MongoClient.connect(url)
   .catch(err => {
     console.error('몽고DB 접속 실패:', err);
   });
+
+
