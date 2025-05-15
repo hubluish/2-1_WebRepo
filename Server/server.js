991f9bbf5c4ae60f33c91bd211a6eb2f8348e1cf
hubluish <james2048@naver.com>
2025-05-15 15:27:44 +0900

    week 11 The End

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,14 @@ const path = require('path');
 const mongoclient = require("mongodb").MongoClient;
 const ObjId = require('mongodb').ObjectId;
 
+// body-parser 라이브러리 추가
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+// 정적 파일 라이브러리 추가
+app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
 const db = require('node-mysql/lib/db');
 app.set('view engine', 'ejs');
@@ -20,6 +28,10 @@ MongoClient.connect(url)
 
     mydb = client.db('myboard');
 
+    app.get('/', function (req, res) {
+      res.render('index.ejs');
+    });
+
     app.get('/enter', function (req, res) {
       res.render('enter.ejs');
     });
@@ -29,27 +41,47 @@ MongoClient.connect(url)
     res.render('list.ejs', { data: result });
   });
 
-
-
-
-
-
-
-
-    app.post('/save', function(req, res){
-      console.log(req.body.title);
-      console.log(req.body.content);
-      console.log(req.body.someDate);
-  // 몽고DB에 데이터 저장하기
-    mydb.collection('post').insertOne(
-      { title: req.body.title, content: req.body.content, date: req.body.someDate }
-    ).then(result => {
+  app.get("/content/:id", function (req, res) {
+  console.log(req.params.id);
+  req.params.id = new ObjId(req.params.id);
+  mydb
+    .collection("post")
+    .findOne({ _id: req.params.id })
+    .then((result) => {
       console.log(result);
-      console.log('데이터 추가 성공');
+      res.render("content.ejs", { data: result });
+    });
+  });
+
+  app.get('/edit/:id', function(req, res){
+  req.params.id = new ObjId(req.params.id);
+  mydb
+    .collection("post")
+    .findOne({ _id: req.params.id })
+    .then((result) => {
+      console.log(result);
+      res.render("edit.ejs", { data: result });
+    });
+});
+
+
+    // '/save' 요청에 대한 post 방식의 처리 루틴
+    app.post("/save", function (req, res) {
+      mydb
+        .collection("post")
+        .insertOne({
+          title: req.body.title,
+          content: req.body.content,
+          date: req.body.someDate,
+        })
+        .then((result) => {
+          console.log(result);
+          console.log("데이터 추가 성공");
+        });
+
+      res.redirect("/list");
     });
 
-    res.send('데이터 추가 성공');
-    });
 
     app.post("/delete", function (req, res) {
   console.log(req.body._id);
@@ -65,6 +97,29 @@ MongoClient.connect(url)
       });
     });
 
+    app.post("/edit", function (req, res) {
+    console.log(req.body);
+      req.body.id = new ObjId(req.body.id);
+      mydb
+        .collection("post")
+        .updateOne(
+          { _id: req.body.id },
+          {
+            $set: {
+              title: req.body.title,
+              content: req.body.content,
+              date: req.body.someDate
+            }
+          }
+        )
+        .then((result) => {
+          console.log("수정완료");
+          res.redirect("/list");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
 
 
     // 서버 시작
