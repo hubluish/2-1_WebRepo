073952f0a180eca0ab38f685ab3ef6160613afb8
hubluish <james2048@naver.com>
2025-05-22 10:50:35 +0900

    12주차

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ const { MongoClient } = require('mongodb');
 const path = require('path');
 const mongoclient = require("mongodb").MongoClient;
 const ObjId = require('mongodb').ObjectId;
+const sha = require('sha256');
 
 // body-parser 라이브러리 추가
 const bodyParser = require("body-parser");
@@ -27,10 +28,6 @@ MongoClient.connect(url)
     console.log('몽고DB 접속 성공');
 
     mydb = client.db('myboard');
-
-    app.get('/', function (req, res) {
-      res.render('index.ejs');
-    });
 
     app.get('/enter', function (req, res) {
       res.render('enter.ejs');
@@ -62,7 +59,108 @@ MongoClient.connect(url)
       console.log(result);
       res.render("edit.ejs", { data: result });
     });
-});
+  });
+
+  let cookieParser = require("cookie-parser");
+  
+  app.use(cookieParser('ncvka0e398423kpfd'));
+  app.get('/cookie', function(req, res){
+    let milk = parseInt(req.signedCookies.milk) + 1000;
+    if (isNaN(milk)) {
+      milk = 0;
+    }
+    res.cookie('milk', milk, {signed: true});
+    res.send('product :' + milk + '원');
+  });
+
+  let session = require('express-session');
+
+  app.use(session({
+    secret: 'dkufe8938493j4e08349u',
+    resave: false,
+    saveUninitialized: true,
+  }));
+  app.get('/session', function(req, res){
+    if (isNaN(req.session.milk)) {
+      req.session.milk = 0;
+    }
+    req.session.milk = req.session.milk + 1000;
+    res.send("session : " + req.session.milk + "원");
+  });
+
+  app.get('/login', function(req, res){
+    console.log(req.session);
+    if (req.session.user) {
+      console.log('세션 유지');
+      res.render('index.ejs', {user: req.session.user});
+    }else{
+      res.render('login.ejs');
+    }
+  });
+
+  app.post("/login", function (req, res) {
+    console.log("아이디 : " + req.body.userid);
+    console.log("비밀번호 : " + req.body.userpw);
+
+    mydb
+      .collection("account")
+      .findOne({ userid: req.body.userid })
+      .then((result) => {
+        if (result.userpw == sha(req.body.userpw)) {
+          req.session.user = req.body;
+          console.log("새로운 로그인");
+          res.render("index.ejs", { user: req.session.user });
+        } else {
+          res.render("login.ejs");
+        }
+      });
+  });
+
+
+    app.get('/logout', function(req, res){
+      console.log('로그아웃');
+      req.session.destroy();
+      res.render('index.ejs', {user: null});
+    });
+
+    app.get('/signup', function(req, res){
+      res.render('signup.ejs');
+    });
+
+    app.get("/", function (req, res) {
+    // res.render("index.ejs");
+    if (req.session.user) {
+      console.log("세션 유지");
+      // res.send('로그인 되었습니다.');
+      res.render("index.ejs", { user: req.session.user });
+    } else {
+      console.log("user : null");
+      res.render("index.ejs", { user: null });
+    }
+  });
+
+    app
+
+    app.post("/signup", function (req, res) {
+      console.log(req.body.userid);
+      console.log(req.body.userpw);
+      console.log(req.body.usergroup);
+      console.log(req.body.useremail);
+
+      mydb
+        .collection("account")
+        .insertOne({
+          userid: req.body.userid,
+          userpw: sha(req.body.userpw),
+          usergroup: req.body.usergroup,
+          useremail: req.body.useremail,
+        })
+        .then((result) => {
+          console.log("회원가입 성공");
+        });
+
+      res.redirect("/");
+    });
 
 
     // '/save' 요청에 대한 post 방식의 처리 루틴
@@ -120,6 +218,7 @@ MongoClient.connect(url)
           console.log(err);
         });
     });
+
 
 
     // 서버 시작
